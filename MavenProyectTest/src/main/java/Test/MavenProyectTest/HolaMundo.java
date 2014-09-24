f8dabf706c1c9356519f8c0a220a9b4f2a29d23d
capemo <E205-26@172.16.14.15>
2014-09-24 08:52:14 -0500

    ...>>>>---

---
+++ MavenProyectTest/src/main/java/Test/MavenProyectTest/HolaMundo.java
@@ -0,0 +1,14 @@
+package Test.MavenProyectTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class HolaMundo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo >.<" );
+        System.out.println( "Hola Mundo**" );
+    }
+}
