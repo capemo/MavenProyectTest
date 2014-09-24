76f3d6102df6cb51e1ce97d57d75f5dff34ed793
Cristian2554 <corpari25@gmail.com>
2014-09-24 08:44:39 -0500

    Commit Cristian

--- MavenProyectTest/src/main/java/Test/MavenProyectTest/HolaMundo.java
+++ MavenProyectTest/src/main/java/Test/MavenProyectTest/HolaMundo.java
@@ -8,6 +8,6 @@ public class HolaMundo
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!! att: Cristinmhgan" );
+        System.out.println( "Hola Mundo!! att: Casdristinmhgan" );
     }
 }
